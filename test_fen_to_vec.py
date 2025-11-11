c3dd18df555451cfadb95fbf7df7776277a7483d
Filip Müller <mullefi6@fit.cvut.cz>
2025-11-11 23:06:52 +0100

    Update tests for fen_to_vec

--- test_fen_to_vec.py
+++ test_fen_to_vec.py
@@ -1,19 +1,62 @@
 import unittest
+import torch
 
-from fen_to_vec import fen_to_vec
+import fen_to_vec
 
 
 class TestFenToVec(unittest.TestCase):
+    starting_pieces = [fen_to_vec.ROOK, fen_to_vec.KNIGHT, fen_to_vec.BISHOP, fen_to_vec.QUEEN, fen_to_vec.KING, fen_to_vec.BISHOP, fen_to_vec.KNIGHT, fen_to_vec.ROOK]
+    row_of_pawns = [fen_to_vec.PAWN] * 8
+    empty_row = [fen_to_vec.EMPTY] * 8
+
+    @staticmethod
+    def _apply_color(pieces, color_val):
+        """Returns a list like [pieces[0], color_val, pieces[1], color_val, ...]"""
+        return [e for piece in pieces for e in (piece, color_val)]
+
     def test_starting(self):
         fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-        res = fen_to_vec(fen)
-        print(res)
+        res = fen_to_vec.fen_to_vec(fen)
 
-        expected = [104, 102, 103, 105, 106, 103, 102, 104] + [101] * 8 + [0] * 32 + [1] * 8 + [4, 2, 3, 5, 6, 3, 2, 4]
+        expected = self._apply_color(self.starting_pieces, fen_to_vec.COLOR_NOT_TO_MOVE)
+        expected += self._apply_color(self.row_of_pawns, fen_to_vec.COLOR_NOT_TO_MOVE)
+        expected += 4 * self._apply_color(self.empty_row, fen_to_vec.NEUTRAL)
+        expected += self._apply_color(self.row_of_pawns, fen_to_vec.COLOR_TO_MOVE)
+        expected += self._apply_color(self.starting_pieces, fen_to_vec.COLOR_TO_MOVE)
+
         # castling
-        expected += [1, 1, 1, 1]
+        expected += [fen_to_vec.CAN_CASTLE] * 4
 
-        self.assertEqual(res, expected)
+        expected = torch.Tensor(expected)
+        self.assertTrue(torch.allclose(res, expected))
+
+    def test_starting_black_to_move(self):
+        fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1"
+        res = fen_to_vec.fen_to_vec(fen)
+
+        expected = self._apply_color(self.starting_pieces, fen_to_vec.COLOR_TO_MOVE)
+        expected += self._apply_color(self.row_of_pawns, fen_to_vec.COLOR_TO_MOVE)
+        expected += 4 * self._apply_color(self.empty_row, fen_to_vec.NEUTRAL)
+        expected += self._apply_color(self.row_of_pawns, fen_to_vec.COLOR_NOT_TO_MOVE)
+        expected += self._apply_color(self.starting_pieces, fen_to_vec.COLOR_NOT_TO_MOVE)
+
+        # castling
+        expected += [fen_to_vec.CAN_CASTLE] * 4
+
+        expected = torch.Tensor(expected)
+        self.assertTrue(torch.allclose(res, expected))
+
+    def test_castling(self):
+        fen = "8/8/8/8/8/8/8/8 b Qkq - 0 1"
+        res = fen_to_vec.fen_to_vec(fen)
+
+        expected = 8 * self._apply_color(self.empty_row, fen_to_vec.NEUTRAL)
+
+        # castling
+        expected += [fen_to_vec.CAN_CASTLE] * 2 + [fen_to_vec.CANNOT_CASTLE] + [fen_to_vec.CAN_CASTLE]
+
+        expected = torch.Tensor(expected)
+        self.assertTrue(torch.allclose(res, expected))
 
 
 if __name__ == "__main__":
